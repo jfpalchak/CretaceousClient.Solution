f710b9b249234c02679ec122794d9bc28b32ea0a
Joey Palchak <joey.palchak@gmail.com>
2023-10-24 20:10:35 -0700

    add Details action to Controller + fix typos

--- CretaceousClient/Controllers/AnimalsController.cs
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -5,11 +5,17 @@ namespace CretaceousClient.Controllers;
 
 public class AnimalsController : Controller
 {
-  
+
   public IActionResult Index()
   {
     List<Animal> animals = Animal.GetAnimals();
     
     return View(animals);
+  }
+
+  public IActionResult Details(int id)
+  {
+    Animal animal = Animal.GetDetails(id);
+    return View(animal);
   }
 }
--- CretaceousClient/Models/ApiHelper.cs
+++ CretaceousClient/Models/ApiHelper.cs
@@ -17,7 +17,7 @@ public class ApiHelper
   public static async Task<string> Get(int id)
   {
     RestClient client = new RestClient("http://localhost:5000/");
-    RestRequest request = new RestRequest($"api/animals{id}", Method.Get);
+    RestRequest request = new RestRequest($"api/animals/{id}", Method.Get);
     RestResponse response = await client.GetAsync(request);
 
     return response.Content;
