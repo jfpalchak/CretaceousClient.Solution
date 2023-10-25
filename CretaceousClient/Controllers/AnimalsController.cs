4840b9634a536aa8bd096986e86554ff6ad57282
Joey Palchak <joey.palchak@gmail.com>
2023-10-24 19:50:16 -0700

    create AnimalsController + Index view & action

---
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using CretaceousClient.Models;
+
+namespace CretaceousClient.Controllers;
+
+public class AnimalsController : Controller
+{
+  
+  public IActionResult Index()
+  {
+    List<Animal> animals = Animal.GetAnimals();
+    
+    return View(animals);
+  }
+}
